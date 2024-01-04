d872310cf8c01aae392d49b2bb82f53fb3870e6d
And-ers <accornect@mun.ca>
2024-01-04 14:14:19 -0330

    new file:   .vscode/settings.json 	new file:   __pycache__/twl.cpython-312.pyc 	new file:   twl.py 	new file:   word-graphs/twl_4_word_graph.gexf 	modified:   wordladders.py

--- wordladders.py
+++ wordladders.py
@@ -1,7 +1,9 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 import enchant
+import twl
 import itertools
+import os.path
 
 def countSameLetters(word1, word2):
     count = 0
@@ -10,19 +12,26 @@ def countSameLetters(word1, word2):
             count += 1
     return count
 
-d1 = enchant.Dict("en_US")
-d2 = enchant.Dict("en_UK")
+def generateWordGraph(dic, letters):
+    path_string = './word-graphs/' + dic + "_" + str(letters) + "_word_graph.gexf"
+    if os.path.isfile(path_string):
+        return nx.read_gexf(path_string)
+    else:
+        if dic == 'twl':
+            all_words = [''.join(word) for word in itertools.product('abcdefghijklmnopqrstuvwxyz', repeat=letters) if twl.check(''.join(word))]
+        else:
+            d = enchant.Dict(dic)
+        G = nx.Graph()
+        G.add_nodes_from(all_words)
+        for node1 in G:
+            for node2 in G:
+                if countSameLetters(node1, node2) == letters - 1:
+                    G.add_edge(node1, node2)
+        nx.write_gexf(G, path_string)
+        return G
 
-four_letter_words = [''.join(word) for word in itertools.product('abcdefghijklmnopqrstuvwxyz', repeat=4) if d1.check(''.join(word)) or d2.check(''.join(word))]
+word_graph = generateWordGraph('twl', 4)
 
-G = nx.Graph()
-G.add_nodes_from(four_letter_words)
-
-for node1 in G:
-    for node2 in G:
-        if countSameLetters(node1, node2) == 3:
-            G.add_edge(node1, node2)
-
-print(nx.number_connected_components(G))
-nx.draw(G)
+print(nx.number_connected_components(word_graph))
+nx.draw_networkx(word_graph, pos = nx.kamada_kawai_layout(word_graph), with_labels = True, node_size = 5, font_size = 3, width = 0.5)
 plt.show()
